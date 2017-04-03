3e55a35f019d20d7e1c2c9935d847e18ca76a75e
xiongamao <onycat@live.cn>
2017-04-03 20:29:00 +0800

    add JS practice

--- JS-exercise/assert-exercise.js
+++
@@ -1,6 +0,0 @@
-function assert(value,description){
-    description = description || ''
-    value === true? (console.info('[TEST PASS]'+ description)): (console.error('[Test failed]'+ description))
-}
-
-assert(1===1,'1等于1')
